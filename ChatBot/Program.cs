c80760690c5167f4022d9651060ffe11a70eaa18
BreZzy33 <kenni1@live.ru>
2017-08-03 18:47:27 +0300

    Create new app for chat-bot.

---
+++ ChatBot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ChatBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your answer:");
+            string s = Console.ReadLine();
+            Console.WriteLine("Bot answer:" + Ans(q)+"/n");
+        }
+
+        private static string Ans(string q)
+        {
+            
+        }
+    }
+}
