9c5c0f8110540ba789e1e7d296238bea956eacb6
BreZzy33 <kenni1@live.ru>
2017-08-03 19:03:01 +0300

    Wrote all base cod for simple chat.

--- ChatBot/Program.cs
+++ ChatBot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ChatBot
 {
@@ -13,7 +14,36 @@ namespace ChatBot
 
         private static string Ans(string q)
         {
-            
+            string tr = ")(:^^=!?", //Simbols that should delete
+                ans = ""; //Bots answers
+
+            q = q.ToLower(); //Translate to lower register
+            q = Trim(q, tr.ToCharArray()); //Delete letters
+            string[] baza = File.ReadAllLines("1.txt");
+
+            //Searching
+            for (int i = 0; i < baza.Length; i += 2) 
+            {
+                if (q==baza[i])
+                {
+                    ans = baza[i + 1];
+                    break;
+                }   
+            }
+            return ans;
+        }
+
+        //Delete letters
+        private static string Trim(string str, char[] chars)
+        {
+            string stra = str; //Coppy line (do not correct)
+
+            //Delete symbols
+            for (int i=0;i<chars.Length;i++)
+            {
+                stra = stra.Replace(char.ToString(chars[i]), "");
+            }
+            return stra;
         }
     }
 }
