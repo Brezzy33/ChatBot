8aec73effa4ced8174f181a6064c0742ba9b51c8
BreZzy33 <kenni1@live.ru>
2017-08-04 04:37:28 +0300

    Bot now can learn yourself with user help.

---
+++ ChatBot/ChatBot.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ChatBot
+{
+    internal class ChatBot
+    {
+        string q, //Question
+        path, //Way
+        userAnswer; //Users answer(for education)
+        List<string> sempls = new List<string>(); //Answers-Questions (Base)
+        bool flag = true; //Switch education/work
+
+        public event Action<string> GetStr; //Event when enter new text
+
+        public ChatBot(string pat)
+        {
+            path = pat; //Way
+
+            try
+            {
+                sempls.AddRange(File.ReadAllLines(path)); //Try add base
+            }
+            catch
+            {
+
+            }
+
+            GetStr += ChatBot_GetStr;
+            GetStr("\n Your answer:");
+        }
+
+        //Delete letters
+        static string Trim(string str, char[] chars)
+        {
+            string strA = str; //Coppy line (do not correct)
+
+            //Delete symbols
+            for (int i = 0; i < chars.Length; i++)
+            {
+                strA = strA.Replace(char.ToString(chars[i]), "");
+            }
+            return strA;
+        }
+
+        public void Ans(string qw)
+        {
+            if (flag)
+            {
+                q = qw;
+                string ans = Answer(qw);
+
+                if (ans == string.Empty)
+                {
+                    flag = false;
+                    GetStr("Enter answer: ");
+                }
+                else GetStr("Bots answer: " + ans + "\n\nYour answer:");
+            }
+            //Education
+            else
+            {
+                flag = true;
+                userAnswer = qw;
+                Teach();
+                GetStr("\n Enter answer: ");
+            }
+        }
+
+        /// <summary>
+        /// Answer
+        /// </summary>
+        /// <param name="quest">Question</param>
+        /// <returns></returns>
+        private string Answer(string qw)
+        {
+            string tr = ")(:^^=!?", //Simbols that should delete
+                ans = string.Empty; //Bots answers
+
+            qw = qw.ToLower(); //Translate to lower register
+            qw = Trim(qw, tr.ToCharArray()); //Delete letters 
+
+            //Searching
+            for (int i = 0; i < sempls.Count; i += 2)
+            {
+                if (qw == sempls[i])
+                {
+                    ans = sempls[i + 1]; //Return answer
+                    break; //End cycle
+                }
+            }
+            return ans; //Answer
+        }
+
+        /// <summary>
+        /// Teach
+        /// </summary>
+        private void Teach()
+        {
+            sempls.Add(q); //Add question
+            sempls.Add(userAnswer); //Answer
+            File.WriteAllLines(path, sempls.ToArray()); //Save
+        }
+
+        void ChatBot_GetStr(string obj) //Stug or Cap for event (if event = empty)
+        {
+
+        }
+    }
+}
--- ChatBot/Program.cs
+++ ChatBot/Program.cs
@@ -1,54 +1,25 @@
 ﻿using System;
-using System.IO;
 
 namespace ChatBot
 {
     class Program
     {
-
-        //Delete letters
-        static string Trim(string str, char[] chars)
-        {
-            string strA = str; //Coppy line (do not correct)
-
-            //Delete symbols
-            for (int i = 0; i < chars.Length; i++)
-            {
-                strA = strA.Replace(char.ToString(chars[i]), "");
-            }
-            return strA;
-        }
-
-        static string Ans(string quest)
-        {
-            string tr = ")(:^^=!?", //Simbols that should delete
-                ans = ""; //Bots answers
-
-            quest = quest.ToLower(); //Translate to lower register
-            quest = Trim(quest, tr.ToCharArray()); //Delete letters
-            string[] baza = File.ReadAllLines("D:\\C#\\ChatBot\\ChatBot\\bin\\Debug\\1.txt");
-
-            //Searching
-            for (int i = 0; i < baza.Length; i += 2)
-            {
-                if (quest == baza[i])
-                {
-                    ans = baza[i + 1]; //Return answer
-                    break; //End cycle
-                }
-            }
-            return ans; //Answer
-        }
-
         public static void Main()
         {
+            ChatBot bot = new ChatBot(@"D:\\C#\\ChatBot\\ChatBot\\bin\\Debug\\1.txt"); //Create bot
+            bot.GetStr += bot_GetStr; //Subscribe to action
+
             //Endless cycle
             while (true)
             {
-                Console.Write("Your answer:");
-                string quest = Console.ReadLine();
-                Console.WriteLine("Bot answered: " + Ans(quest) + "\n");
+                string q = Console.ReadLine(); //Enter answer
+                bot.Ans(q);
             }
-        } 
+        }
+
+        static void bot_GetStr(string obj)
+        {
+            Console.WriteLine(obj); //Show answer
+        }
     }
 }
